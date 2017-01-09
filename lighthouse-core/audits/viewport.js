f86315ac9d5f763ccc768a97ba1fabd703d6d12f
Matthew Somerville <matthew-github@dracos.co.uk>
2017-01-09 13:01:42 +0000

    fixup! squash! Allow initial-scale and spaces in viewport test.

--- lighthouse-core/audits/viewport.js
+++ lighthouse-core/audits/viewport.js
@@ -50,11 +50,12 @@ class Viewport extends Audit {
     const parsedProps = Parser.parseMetaViewPortContent(artifacts.Viewport);
 
     if (Object.keys(parsedProps.unknownProperties).length) {
-      debugString += `Invalid properties found: ${parsedProps.unknownProperties}`;
+      debugString += `Invalid properties found: ${parsedProps.unknownProperties}. `;
     }
     if (Object.keys(parsedProps.invalidValues).length) {
-      debugString += `Invalid values found: ${parsedProps.invalidValues}`;
+      debugString += `Invalid values found: ${parsedProps.invalidValues}. `;
     }
+    debugString = debugString.trim();
 
     const viewportProps = parsedProps.validProperties;
     const hasMobileViewport = viewportProps.width || viewportProps['initial-scale'];
